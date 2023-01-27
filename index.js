a3abd11cd4f726dc0a40762a064ffb48e2c12f0b
Kailen James <sevenflags57@gmail.com>
2023-01-27 11:25:09 -0800

    Finished up application

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const db = {
 	nextID: 0,
 	data: (() => {
@@ -11,30 +12,37 @@ const db = {
 			return newElem;
 		});
 	})(),
-	get: function (id) {
-		if (id) {
-			return null;
-		} else {
-			return this.data;
-		}
+	get: function () {
+		return this.data;
 	},
-	add: function (entry) {
+	add: async function (entry) {
 		entry.id = this.nextID++;
 		this.data.push(entry);
-		return entry;
+		try {
+			await fs.promises.writeFile("./db/db.json", JSON.stringify(this.data));
+			return entry;
+		} catch(e) {
+			return null;
+		}
 	},
-	remove: function (id) {
+	remove: async function (id) {
+		if(typeof id !== 'number') throw new Error(`id must be a number, was a ${typeof id}`);
 		const length = this.data.length;
 		for (let i = 0; i < length; ++i) {
 			if (this.data[i].id === id) {
-				const target = this.data[i];
-				this.data.splice(i, 1);
-				return target;
+				try {
+					const target = this.data[i];
+					this.data.splice(i, 1);
+					await fs.promises.writeFile("./db/db.json", JSON.stringify(this.data));
+					return target;
+				} catch(e) {
+					return null;
+				}
 			}
 		}
 		return null;
-	}
-}
+	},
+};
 
 const PORT = 3000;
 const app = express();
@@ -46,12 +54,12 @@ app.get("/api/notes", (req, res) => {
 	res.json(db.get());
 });
 
-app.post("/api/notes", (req, res) => {
-	res.json(db.add(req.body));
+app.post("/api/notes", async (req, res) => {
+	res.json(await db.add(req.body));
 });
 
-app.delete("/api/notes/:id", (req, res) => {
-	res.json(db.remove(parseInt(req.params.id)));
+app.delete("/api/notes/:id", async (req, res) => {
+	res.json(await db.remove(parseInt(req.params.id)));
 });
 
 app.get("/notes", (req, res) => {
