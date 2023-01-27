333d26d4730e72489c4680a6ac7a4a3e7a6a74c6
Kailen James <sevenflags57@gmail.com>
2023-01-27 10:52:55 -0800

    Added all routes

--- index.js
+++ index.js
@@ -5,14 +5,26 @@ const PORT = 3000;
 const app = express();
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "view", "index.html"));
+app.get("/api/notes", (req, res) => {
+	res.send("Get");
+});
+
+app.post("/api/notes", (req, res) => {
+	res.send("Post");
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+	res.send("Delete");
 });
 
 app.get("/notes", (req, res) => {
 	res.sendFile(path.join(__dirname, "view", "notes.html"));
+});
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "view", "index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
